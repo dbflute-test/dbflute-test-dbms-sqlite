02899b589470ae8cf83b8eb02cfbbfa396a5a475
jflute <dbflute@gmail.com>
2014-11-18 12:57:14 +0900

    pilotD

---
+++ src/main/java/org/docksidestage/sqlite/dbflute/bsentity/BsPurchasePayment.java
@@ -0,0 +1,404 @@
+package org.docksidestage.sqlite.dbflute.bsentity;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import org.dbflute.dbmeta.DBMeta;
+import org.dbflute.dbmeta.AbstractEntity;
+import org.dbflute.dbmeta.accessory.DomainEntity;
+import org.docksidestage.sqlite.dbflute.allcommon.DBMetaInstanceHandler;
+import org.docksidestage.sqlite.dbflute.exentity.*;
+
+/**
+ * The entity of PURCHASE_PAYMENT as TABLE. <br>
+ * <pre>
+ * [primary-key]
+ *     PURCHASE_PAYMENT_ID
+ * 
+ * [column]
+ *     PURCHASE_PAYMENT_ID, PURCHASE_ID, PAYMENT_AMOUNT, PAYMENT_DATETIME, PAYMENT_METHOD_CODE, REGISTER_DATETIME, REGISTER_USER, REGISTER_PROCESS, UPDATE_DATETIME, UPDATE_USER, VERSION_NO
+ * 
+ * [sequence]
+ *     
+ * 
+ * [identity]
+ *     PURCHASE_PAYMENT_ID
+ * 
+ * [version-no]
+ *     VERSION_NO
+ * 
+ * [foreign table]
+ *     
+ * 
+ * [referrer table]
+ *     
+ * 
+ * [foreign property]
+ *     
+ * 
+ * [referrer property]
+ *     
+ * 
+ * [get/set template]
+ * /= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+ * Integer purchasePaymentId = entity.getPurchasePaymentId();
+ * Integer purchaseId = entity.getPurchaseId();
+ * String paymentAmount = entity.getPaymentAmount();
+ * java.time.LocalDateTime paymentDatetime = entity.getPaymentDatetime();
+ * String paymentMethodCode = entity.getPaymentMethodCode();
+ * java.time.LocalDateTime registerDatetime = entity.getRegisterDatetime();
+ * String registerUser = entity.getRegisterUser();
+ * String registerProcess = entity.getRegisterProcess();
+ * java.time.LocalDateTime updateDatetime = entity.getUpdateDatetime();
+ * String updateUser = entity.getUpdateUser();
+ * Integer versionNo = entity.getVersionNo();
+ * entity.setPurchasePaymentId(purchasePaymentId);
+ * entity.setPurchaseId(purchaseId);
+ * entity.setPaymentAmount(paymentAmount);
+ * entity.setPaymentDatetime(paymentDatetime);
+ * entity.setPaymentMethodCode(paymentMethodCode);
+ * entity.setRegisterDatetime(registerDatetime);
+ * entity.setRegisterUser(registerUser);
+ * entity.setRegisterProcess(registerProcess);
+ * entity.setUpdateDatetime(updateDatetime);
+ * entity.setUpdateUser(updateUser);
+ * entity.setVersionNo(versionNo);
+ * = = = = = = = = = =/
+ * </pre>
+ * @author DBFlute(AutoGenerator)
+ */
+public abstract class BsPurchasePayment extends AbstractEntity implements DomainEntity {
+
+    // ===================================================================================
+    //                                                                          Definition
+    //                                                                          ==========
+    /** The serial version UID for object serialization. (Default) */
+    private static final long serialVersionUID = 1L;
+
+    // ===================================================================================
+    //                                                                           Attribute
+    //                                                                           =========
+    /** PURCHASE_PAYMENT_ID: {PK, ID, NotNull, INTEGER(2000000000, 10)} */
+    protected Integer _purchasePaymentId;
+
+    /** PURCHASE_ID: {NotNull, INTEGER(2000000000, 10)} */
+    protected Integer _purchaseId;
+
+    /** PAYMENT_AMOUNT: {NotNull, DECIMAL(10,2)(2000000000, 10)} */
+    protected String _paymentAmount;
+
+    /** PAYMENT_DATETIME: {NotNull, DATETIME(2000000000, 10)} */
+    protected java.time.LocalDateTime _paymentDatetime;
+
+    /** PAYMENT_METHOD_CODE: {NotNull, CHAR(3)(2000000000, 10)} */
+    protected String _paymentMethodCode;
+
+    /** REGISTER_DATETIME: {NotNull, DATETIME(2000000000, 10)} */
+    protected java.time.LocalDateTime _registerDatetime;
+
+    /** REGISTER_USER: {NotNull, VARCHAR(200)(2000000000, 10)} */
+    protected String _registerUser;
+
+    /** REGISTER_PROCESS: {NotNull, TEXT(2000000000, 10)} */
+    protected String _registerProcess;
+
+    /** UPDATE_DATETIME: {NotNull, DATETIME(2000000000, 10)} */
+    protected java.time.LocalDateTime _updateDatetime;
+
+    /** UPDATE_USER: {NotNull, VARCHAR(200)(2000000000, 10)} */
+    protected String _updateUser;
+
+    /** VERSION_NO: {NotNull, INTEGER(2000000000, 10)} */
+    protected Integer _versionNo;
+
+    // ===================================================================================
+    //                                                                             DB Meta
+    //                                                                             =======
+    /** {@inheritDoc} */
+    public DBMeta asDBMeta() {
+        return DBMetaInstanceHandler.findDBMeta(asTableDbName());
+    }
+
+    /** {@inheritDoc} */
+    public String asTableDbName() {
+        return "PURCHASE_PAYMENT";
+    }
+
+    // ===================================================================================
+    //                                                                         Primary Key
+    //                                                                         ===========
+    /** {@inheritDoc} */
+    public boolean hasPrimaryKeyValue() {
+        if (_purchasePaymentId == null) { return false; }
+        return true;
+    }
+
+    // ===================================================================================
+    //                                                                    Foreign Property
+    //                                                                    ================
+    // ===================================================================================
+    //                                                                   Referrer Property
+    //                                                                   =================
+    protected <ELEMENT> List<ELEMENT> newReferrerList() {
+        return new ArrayList<ELEMENT>();
+    }
+
+    // ===================================================================================
+    //                                                                      Basic Override
+    //                                                                      ==============
+    @Override
+    protected boolean doEquals(Object obj) {
+        if (obj instanceof BsPurchasePayment) {
+            BsPurchasePayment other = (BsPurchasePayment)obj;
+            if (!xSV(_purchasePaymentId, other._purchasePaymentId)) { return false; }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    protected int doHashCode(int initial) {
+        int hs = initial;
+        hs = xCH(hs, asTableDbName());
+        hs = xCH(hs, _purchasePaymentId);
+        return hs;
+    }
+
+    @Override
+    protected String doBuildStringWithRelation(String li) {
+        return "";
+    }
+
+    @Override
+    protected String doBuildColumnString(String dm) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(dm).append(xfND(_purchasePaymentId));
+        sb.append(dm).append(xfND(_purchaseId));
+        sb.append(dm).append(xfND(_paymentAmount));
+        sb.append(dm).append(xfND(_paymentDatetime));
+        sb.append(dm).append(xfND(_paymentMethodCode));
+        sb.append(dm).append(xfND(_registerDatetime));
+        sb.append(dm).append(xfND(_registerUser));
+        sb.append(dm).append(xfND(_registerProcess));
+        sb.append(dm).append(xfND(_updateDatetime));
+        sb.append(dm).append(xfND(_updateUser));
+        sb.append(dm).append(xfND(_versionNo));
+        if (sb.length() > dm.length()) {
+            sb.delete(0, dm.length());
+        }
+        sb.insert(0, "{").append("}");
+        return sb.toString();
+    }
+
+    @Override
+    protected String doBuildRelationString(String dm) {
+        return "";
+    }
+
+    @Override
+    public PurchasePayment clone() {
+        return (PurchasePayment)super.clone();
+    }
+
+    // ===================================================================================
+    //                                                                            Accessor
+    //                                                                            ========
+    /**
+     * [get] PURCHASE_PAYMENT_ID: {PK, ID, NotNull, INTEGER(2000000000, 10)} <br>
+     * @return The value of the column 'PURCHASE_PAYMENT_ID'. (basically NotNull if selected: for the constraint)
+     */
+    public Integer getPurchasePaymentId() {
+        checkSpecifiedProperty("purchasePaymentId");
+        return _purchasePaymentId;
+    }
+
+    /**
+     * [set] PURCHASE_PAYMENT_ID: {PK, ID, NotNull, INTEGER(2000000000, 10)} <br>
+     * @param purchasePaymentId The value of the column 'PURCHASE_PAYMENT_ID'. (basically NotNull if update: for the constraint)
+     */
+    public void setPurchasePaymentId(Integer purchasePaymentId) {
+        registerModifiedProperty("purchasePaymentId");
+        _purchasePaymentId = purchasePaymentId;
+    }
+
+    /**
+     * [get] PURCHASE_ID: {NotNull, INTEGER(2000000000, 10)} <br>
+     * @return The value of the column 'PURCHASE_ID'. (basically NotNull if selected: for the constraint)
+     */
+    public Integer getPurchaseId() {
+        checkSpecifiedProperty("purchaseId");
+        return _purchaseId;
+    }
+
+    /**
+     * [set] PURCHASE_ID: {NotNull, INTEGER(2000000000, 10)} <br>
+     * @param purchaseId The value of the column 'PURCHASE_ID'. (basically NotNull if update: for the constraint)
+     */
+    public void setPurchaseId(Integer purchaseId) {
+        registerModifiedProperty("purchaseId");
+        _purchaseId = purchaseId;
+    }
+
+    /**
+     * [get] PAYMENT_AMOUNT: {NotNull, DECIMAL(10,2)(2000000000, 10)} <br>
+     * @return The value of the column 'PAYMENT_AMOUNT'. (basically NotNull if selected: for the constraint)
+     */
+    public String getPaymentAmount() {
+        checkSpecifiedProperty("paymentAmount");
+        return _paymentAmount;
+    }
+
+    /**
+     * [set] PAYMENT_AMOUNT: {NotNull, DECIMAL(10,2)(2000000000, 10)} <br>
+     * @param paymentAmount The value of the column 'PAYMENT_AMOUNT'. (basically NotNull if update: for the constraint)
+     */
+    public void setPaymentAmount(String paymentAmount) {
+        registerModifiedProperty("paymentAmount");
+        _paymentAmount = paymentAmount;
+    }
+
+    /**
+     * [get] PAYMENT_DATETIME: {NotNull, DATETIME(2000000000, 10)} <br>
+     * @return The value of the column 'PAYMENT_DATETIME'. (basically NotNull if selected: for the constraint)
+     */
+    public java.time.LocalDateTime getPaymentDatetime() {
+        checkSpecifiedProperty("paymentDatetime");
+        return _paymentDatetime;
+    }
+
+    /**
+     * [set] PAYMENT_DATETIME: {NotNull, DATETIME(2000000000, 10)} <br>
+     * @param paymentDatetime The value of the column 'PAYMENT_DATETIME'. (basically NotNull if update: for the constraint)
+     */
+    public void setPaymentDatetime(java.time.LocalDateTime paymentDatetime) {
+        registerModifiedProperty("paymentDatetime");
+        _paymentDatetime = paymentDatetime;
+    }
+
+    /**
+     * [get] PAYMENT_METHOD_CODE: {NotNull, CHAR(3)(2000000000, 10)} <br>
+     * @return The value of the column 'PAYMENT_METHOD_CODE'. (basically NotNull if selected: for the constraint)
+     */
+    public String getPaymentMethodCode() {
+        checkSpecifiedProperty("paymentMethodCode");
+        return _paymentMethodCode;
+    }
+
+    /**
+     * [set] PAYMENT_METHOD_CODE: {NotNull, CHAR(3)(2000000000, 10)} <br>
+     * @param paymentMethodCode The value of the column 'PAYMENT_METHOD_CODE'. (basically NotNull if update: for the constraint)
+     */
+    public void setPaymentMethodCode(String paymentMethodCode) {
+        registerModifiedProperty("paymentMethodCode");
+        _paymentMethodCode = paymentMethodCode;
+    }
+
+    /**
+     * [get] REGISTER_DATETIME: {NotNull, DATETIME(2000000000, 10)} <br>
+     * @return The value of the column 'REGISTER_DATETIME'. (basically NotNull if selected: for the constraint)
+     */
+    public java.time.LocalDateTime getRegisterDatetime() {
+        checkSpecifiedProperty("registerDatetime");
+        return _registerDatetime;
+    }
+
+    /**
+     * [set] REGISTER_DATETIME: {NotNull, DATETIME(2000000000, 10)} <br>
+     * @param registerDatetime The value of the column 'REGISTER_DATETIME'. (basically NotNull if update: for the constraint)
+     */
+    public void setRegisterDatetime(java.time.LocalDateTime registerDatetime) {
+        registerModifiedProperty("registerDatetime");
+        _registerDatetime = registerDatetime;
+    }
+
+    /**
+     * [get] REGISTER_USER: {NotNull, VARCHAR(200)(2000000000, 10)} <br>
+     * @return The value of the column 'REGISTER_USER'. (basically NotNull if selected: for the constraint)
+     */
+    public String getRegisterUser() {
+        checkSpecifiedProperty("registerUser");
+        return _registerUser;
+    }
+
+    /**
+     * [set] REGISTER_USER: {NotNull, VARCHAR(200)(2000000000, 10)} <br>
+     * @param registerUser The value of the column 'REGISTER_USER'. (basically NotNull if update: for the constraint)
+     */
+    public void setRegisterUser(String registerUser) {
+        registerModifiedProperty("registerUser");
+        _registerUser = registerUser;
+    }
+
+    /**
+     * [get] REGISTER_PROCESS: {NotNull, TEXT(2000000000, 10)} <br>
+     * @return The value of the column 'REGISTER_PROCESS'. (basically NotNull if selected: for the constraint)
+     */
+    public String getRegisterProcess() {
+        checkSpecifiedProperty("registerProcess");
+        return _registerProcess;
+    }
+
+    /**
+     * [set] REGISTER_PROCESS: {NotNull, TEXT(2000000000, 10)} <br>
+     * @param registerProcess The value of the column 'REGISTER_PROCESS'. (basically NotNull if update: for the constraint)
+     */
+    public void setRegisterProcess(String registerProcess) {
+        registerModifiedProperty("registerProcess");
+        _registerProcess = registerProcess;
+    }
+
+    /**
+     * [get] UPDATE_DATETIME: {NotNull, DATETIME(2000000000, 10)} <br>
+     * @return The value of the column 'UPDATE_DATETIME'. (basically NotNull if selected: for the constraint)
+     */
+    public java.time.LocalDateTime getUpdateDatetime() {
+        checkSpecifiedProperty("updateDatetime");
+        return _updateDatetime;
+    }
+
+    /**
+     * [set] UPDATE_DATETIME: {NotNull, DATETIME(2000000000, 10)} <br>
+     * @param updateDatetime The value of the column 'UPDATE_DATETIME'. (basically NotNull if update: for the constraint)
+     */
+    public void setUpdateDatetime(java.time.LocalDateTime updateDatetime) {
+        registerModifiedProperty("updateDatetime");
+        _updateDatetime = updateDatetime;
+    }
+
+    /**
+     * [get] UPDATE_USER: {NotNull, VARCHAR(200)(2000000000, 10)} <br>
+     * @return The value of the column 'UPDATE_USER'. (basically NotNull if selected: for the constraint)
+     */
+    public String getUpdateUser() {
+        checkSpecifiedProperty("updateUser");
+        return _updateUser;
+    }
+
+    /**
+     * [set] UPDATE_USER: {NotNull, VARCHAR(200)(2000000000, 10)} <br>
+     * @param updateUser The value of the column 'UPDATE_USER'. (basically NotNull if update: for the constraint)
+     */
+    public void setUpdateUser(String updateUser) {
+        registerModifiedProperty("updateUser");
+        _updateUser = updateUser;
+    }
+
+    /**
+     * [get] VERSION_NO: {NotNull, INTEGER(2000000000, 10)} <br>
+     * @return The value of the column 'VERSION_NO'. (basically NotNull if selected: for the constraint)
+     */
+    public Integer getVersionNo() {
+        checkSpecifiedProperty("versionNo");
+        return _versionNo;
+    }
+
+    /**
+     * [set] VERSION_NO: {NotNull, INTEGER(2000000000, 10)} <br>
+     * @param versionNo The value of the column 'VERSION_NO'. (basically NotNull if update: for the constraint)
+     */
+    public void setVersionNo(Integer versionNo) {
+        registerModifiedProperty("versionNo");
+        _versionNo = versionNo;
+    }
+}
